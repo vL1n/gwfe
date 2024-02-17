36b4623f05d0f01f7b0e506e0f36c7b905f71e76
vL1n Server <vl1n-server@vL1ndeMac-mini.local>
2024-02-18 01:32:17 +0800

    fix: add dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,27 @@
 import { useState } from 'react';
 import logo from '@/assets/logo.png';
+import queryString from 'query-string';
 import styles from './index.module.css';
+
+const {isDark = false} = queryString.parse(location.search);
 
 export default function IndexPage() {
   const [count, setCount] = useState(1);
   const updateCount = () => setCount((c) => c + 1);
 
   return (
-    <div className={styles.app}>
+    <div className={styles.app} style={{ background: isDark ? '#000' : '#fff'}}>
       <header>
         <img src={logo} alt="logo" />
         <p>
           Hello ice.js 3
         </p>
-        <span>{window.location.href}</span>
+        <span style={{color: isDark ? '#fff' : '#000'}}>{window.location.href}</span>
       </header>
       <main>
         <button className={styles.button} type="button" onClick={updateCount}>
           👍🏻 {count}
         </button>
-        <p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.link}
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            href="https://v3.ice.work/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.link}
-          >
-            Learn ice.js
-          </a>
-        </p>
       </main>
     </div>
   );
