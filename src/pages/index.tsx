32b5a31371f2f833af51511e419eda2fb86b1b67
vL1n Server <vl1n-server@vL1ndeMac-mini.local>
2024-02-18 02:06:46 +0800

    fix: isDark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,20 @@ import logo from '@/assets/logo.png';
 import queryString from 'query-string';
 import styles from './index.module.css';
 
-const {isDark = false} = queryString.parse(location.search);
+const {isDark = "false"} = queryString.parse(location.search);
 
 export default function IndexPage() {
   const [count, setCount] = useState(1);
   const updateCount = () => setCount((c) => c + 1);
 
   return (
-    <div className={styles.app} style={{ background: isDark ? '#000' : '#fff'}}>
+    <div className={styles.app} style={{ background: isDark === 'true' ? '#000' : '#fff'}}>
       <header>
         <img src={logo} alt="logo" />
-        <p style={{color: isDark ? '#fff' : '#000'}}>
+        <p style={{color: isDark === 'true' ? '#fff' : '#000'}}>
           Hello ice.js 3
         </p>
-        <span style={{color: isDark ? '#fff' : '#000'}}>{window.location.href}</span>
+        <span style={{color: isDark === 'true' ? '#fff' : '#000'}}>{window.location.href}</span>
       </header>
       <main>
         <button className={styles.button} type="button" onClick={updateCount}>
